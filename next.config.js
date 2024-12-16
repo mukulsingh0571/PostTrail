3d380dae8699e60302d3e1653a4e6b3bfd283e91
Mukul Singh <mukulsingh@Mukuls-MacBook-Air.local>
2024-12-17 01:09:29 +0530

    Fix next.config.js and update configurations

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
-    }
-}
-
-module.exports = nextConfig
-
-
-// https://firebasestorage.googleapis.com/posttrail-378a8.firebasestorage.app/1734367606991f1.jpg
+    images: {
+      domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // Skip ESLint during builds
+    },
+  };
+  
+  module.exports = nextConfig;
+  
